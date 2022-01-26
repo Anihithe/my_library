b8b7644dcb1bc6eb6e80042317c28b09c4dc0c37
Anihithe <anihithe@gmail.com>
2022-01-26 23:14:00 +0100

    setup navigation rail

--- lib/widgets/window_main/window_main.dart
+++ lib/widgets/window_main/window_main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_library/widgets/main_menu/main_menu.dart';
 
 class WindowMain extends StatelessWidget {
   const WindowMain({Key? key}) : super(key: key);
@@ -12,61 +11,76 @@ class WindowMain extends StatelessWidget {
   }
 }
 
-class MainView extends StatelessWidget {
+class MainView extends StatefulWidget {
   const MainView({Key? key}) : super(key: key);
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  int _selectedIndex = 0;
+
+  List list = [
+    const HomePage(text: "Home", color: Colors.red),
+    const HomePage(text: "Book", color: Colors.yellow),
+    const HomePage(text: "Music", color: Colors.blue),
+    const HomePage(text: "Param", color: Colors.green)
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: const [
-        AppMenu(),
-        AppFrame(),
+      children: [
+        NavigationRail(
+          onDestinationSelected: (value) {
+            setState(() {
+              _selectedIndex = value;
+            });
+          },
+          selectedIndex: _selectedIndex,
+          labelType: NavigationRailLabelType.selected,
+          destinations: const [
+            NavigationRailDestination(
+              icon: Icon(Icons.home),
+              label: Text("Home"),
+            ),
+            NavigationRailDestination(
+              icon: Icon(Icons.book),
+              label: Text("Book"),
+            ),
+            NavigationRailDestination(
+              icon: Icon(Icons.music_note),
+              label: Text("Music"),
+            ),
+            NavigationRailDestination(
+              icon: Icon(Icons.settings),
+              label: Text("settings"),
+            ),
+          ],
+        ),
+        Expanded(child: list[_selectedIndex]),
       ],
     );
   }
 }
 
-class AppMenu extends StatefulWidget {
-  const AppMenu({Key? key}) : super(key: key);
-
-  @override
-  State<AppMenu> createState() => _AppMenuState();
-}
-
-class _AppMenuState extends State<AppMenu> {
-  bool isExpanded = true;
-
-  double menuSize = 200;
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key, required this.text, required this.color})
+      : super(key: key);
+  final String text;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: menuSize,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Expanded(
-            child: MainMenu(
-              isExpanded: isExpanded,
-            ),
-          ),
-          TextButton(
-            style: TextButton.styleFrom(
-                minimumSize: const Size.fromHeight(40),
-                alignment: Alignment.centerRight),
-            onPressed: () {
-              setState(() {
-                isExpanded = !isExpanded;
-                menuSize = isExpanded ? 200 : 60;
-              });
-            },
-            child: Icon(
-              isExpanded ? Icons.arrow_back : Icons.arrow_forward,
-              color: Colors.grey,
-            ),
-          )
-        ],
-      ),
+    return Container(
+      color: color,
+      child: Center(
+          child: Text(text,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0))),
     );
   }
 }
